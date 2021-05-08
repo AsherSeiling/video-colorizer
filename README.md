99601409c17bdad243ce16c003e13ee065b8fb44
Asher Seiling <f14asher@gmail.com>
2021-05-08 12:35:18 -0500

    Finished first basic ersion

---
+++ README.md
@@ -0,0 +1,10 @@
+# Video Colorizer
+A python program that uses Deep AIs Image colorize API to colorize vidoes.
+## Usage
+Place the images that you want to colorize into the input folder
+
+Commands
+```
+python3 main.py <Api Key>
+```
+
--- main.py
+++ main.py
@@ -1,6 +1,41 @@
+import json
+import requests
+import sys
+import os
+
+API_KEY = sys.argv[1]
+
+
+# Send to the Api
+def imageColorizeURL(url):
+	r = requests.post("https://api.deepai.org/api/colorizer", files={'image': open(url, 'rb')}, headers={'api-key': API_KEY})
+	return r.json()["output_url"]
+
+# Download image
+def downloadIMG(httpURL, indexURL):
+	os.system(f"curl {httpURL} -o output/{indexURL}")
+
+# Get the images in the input area
+def getImages():
+	dirs = os.listdir("input")
+	finalDir = []
+	for i in dirs:
+		if i.split(".")[1].lower() == "png":
+			finalDir.append(i)
+		elif i.split(".")[1].lower() == "jpg":
+			finalDir.append(i)
+		elif i.split(".")[1].lower() == "jpeg":
+			finalDir.append(i)
+	return finalDir
+
 # Main function
 def main():
-	pass
+	images = getImages()
+	images.sort()
+	for i in images:
+		imageurl = imageColorizeURL(f"input/{i}")
+		print(i)
+		downloadIMG(imageurl, i)
 
-if __name___ == "__main__":
+if __name__ == "__main__":
 	main()
